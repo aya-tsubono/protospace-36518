9b90f3ad6337baaff9512d754ad5c82cddb2f8f5
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-06 19:25:21 +0900

    一覧表示機能の実装

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,6 +4,6 @@ class Prototype < ApplicationRecord
   validates :concept, presence: true
   validates :image, presence: true
 
-  belongs_to :users, optional: true
+  belongs_to :user, optional: true
   has_one_attached :image
 end
