6645e9d5ccf72282a72f3f3927bb91eb688a15cf
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-07 14:40:38 +0900

    削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "prototypes#index"
 
-resources :prototypes, only: [:new, :create, :index, :show, :edit, :update]
+resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
